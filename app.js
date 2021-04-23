cabffe7e99939ccf4e11272551040d08c443b6db
Henil <hnkakadiya2705@gmail.com>
2021-04-23 19:07:09 +0530

    final commit

--- app.js
+++ app.js
@@ -61,17 +61,17 @@ app.use((req, res, next) => {
         .catch(err => next(new Error(err)));
 });
 
-// app.get('/500', errorController.get500);
+app.get('/500', errorController.get500);
 
 app.use(errorController.get404);
 
-// app.use((error, req, res, next) => {
-//     res.status(500).render('500', {
-//         pageTitle: 'Error!',
-//         path: '/500',
-//         isAuthenticated: req.session.isLoggedIn
-//     });
-// });
+app.use((error, req, res, next) => {
+    res.status(500).render('500', {
+        pageTitle: 'Error!',
+        path: '/500',
+        isAuthenticated: req.session.isLoggedIn
+    });
+});
 
 mongoose
     .connect(MONGODB_URI, {
